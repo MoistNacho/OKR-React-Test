ba1b287cd509acbe068a41e104bc5afb1c5c4714
이준성 <junsung.lee@MB-junsunglee.local>
2022-09-27 19:05:53 +0900

    feat: cypress e2e 테스트 작성

---
+++ cypress/e2e/TodoTest.cy.ts
@@ -0,0 +1,49 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
+
+describe("", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  afterEach(() => {
+    cy.wait(1000);
+  });
+
+  it("새로운 아이템 등록 테스트", () => {
+    cy.get('input[placeholder="값을 입력해주세요"]').type("New Item");
+    cy.contains("확인").click();
+
+    cy.get("li").should("have.length", 4);
+    cy.get("li")
+      .last()
+      .children("div")
+      .children("span")
+      .should("have.text", "4. New Item");
+  });
+
+  it("기존 아이템 수정 테스트", () => {
+    cy.contains("수정").first().click();
+
+    cy.get('input[placeholder="아이템 이름을 입력해주세요"]')
+      .clear()
+      .type("Update Item");
+    cy.contains("완료").first().click();
+
+    cy.get("li")
+      .first()
+      .children("div")
+      .children("span")
+      .should("have.text", "1. Update Item");
+  });
+
+  it("기존 아이템 삭제 테스트", () => {
+    cy.contains("삭제").first().click();
+
+    cy.get("li").should("have.length", 2);
+    cy.get("li")
+      .first()
+      .children("div")
+      .children("span")
+      .should("have.text", "1. React");
+  });
+});
